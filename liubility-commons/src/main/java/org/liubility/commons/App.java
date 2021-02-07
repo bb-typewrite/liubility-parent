b08b68e829d954f021f929db6b02faa81dce191c
jdragon <1061917196@qq.com>
2021-02-08 01:42:40 +0800

    init...

---
+++ liubility-commons/src/main/java/org/liubility/commons/App.java
@@ -0,0 +1,10 @@
+package org.liubility.commons;
+
+/**
+ * @Author: Jdragon
+ * @Class: App
+ * @Date: 2021.02.08 上午 1:07
+ * @Description:
+ */
+public class App {
+}
---
+++ liubility-commons/src/main/java/org/liubility/commons/http/HttpUtils.java
@@ -0,0 +1,10 @@
+package org.liubility.commons.http;
+
+/**
+ * @Author: Jdragon
+ * @Class: HttpUtils
+ * @Date: 2021.02.08 上午 1:08
+ * @Description:
+ */
+public class HttpUtils {
+}
