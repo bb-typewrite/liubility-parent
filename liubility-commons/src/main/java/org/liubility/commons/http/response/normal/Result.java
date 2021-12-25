5b0bd02bfd113b29e250aca5cbada1c8375b13c9
zhengjinlong <1061917196@qq.com>
2021-12-25 17:48:26 +0800

    feat: Result Account

--- liubility-commons/src/main/java/org/liubility/commons/http/response/normal/Result.java
+++ liubility-commons/src/main/java/org/liubility/commons/http/response/normal/Result.java
@@ -39,66 +39,75 @@ public class Result<T> implements Serializable {
     private transient static Object defaultData = new Object();
 
 
-    public Result(Long code,String message,T result){
+    public Result(Long code, String message, T result) {
         this.code = code;
         this.message = message;
         this.result = result;
     }
-    public Result(ICode resultCode, T result){
+
+    public Result(ICode resultCode, T result) {
         this.code = resultCode.getCode();
         this.message = resultCode.getMessage();
         this.result = result;
     }
 
-    public static Result<Object> success(){
-        return success(defaultData);
+    @SuppressWarnings("unchecked")
+    public static <T> Result<T> success() {
+        return success((T) defaultData);
     }
 
-    public static Result<String> success(String message){
-        return new Result<>(ResultCode.NORMAL.getCode(),message,message);
+    public static <T> Result<T> success(String message) {
+        return new Result<>(ResultCode.NORMAL.getCode(), message, null);
     }
+
+    @SuppressWarnings("unchecked")
     public static <T> Result<T> success(T result) {
+        if (result == null) {
+            result = (T) defaultData;
+        }
         return new Result<>(ResultCode.NORMAL, result);
     }
+
     public static <T> Result<T> success(String message, T result) {
-        return new Result<>(ResultCode.NORMAL.getCode(), message,result);
+        return new Result<>(ResultCode.NORMAL.getCode(), message, result);
     }
 
+    public static <T> Result<T> error(String message) {
+        return new Result<>(ResultCode.SYSTEM_ERROR.getCode(), message, null);
+    }
 
-    public static Result<Object> error(){
-        return error(defaultData);
-    }
-    public static Result<String> error(String message) {
-        return new Result<>(ResultCode.SYSTEM_ERROR.getCode(),message,null);
-    }
     public static <T> Result<T> error(T result) {
         return new Result<>(ResultCode.SYSTEM_ERROR, result);
     }
+
     public static <T> Result<T> error(String message, T result) {
-        return new Result<>(ResultCode.SYSTEM_ERROR.getCode(), message,result);
+        return new Result<>(ResultCode.SYSTEM_ERROR.getCode(), message, result);
     }
 
     public static <T> Result<T> build(ICode resultCode, T result) {
-        return new Result<>(resultCode,result);
-    }
-    public static <T> Result<T> build(Long code, String message, T result) {
-        return new Result<>(code,message,result);
+        return new Result<>(resultCode, result);
     }
 
-    public static <T> Result<T> authFail(T result){
-        return build(ResultCode.AUTH_FAIL,result);
+    public static <T> Result<T> build(Long code, String message, T result) {
+        return new Result<>(code, message, result);
+    }
+
+    public static <T> Result<T> authFail(T result) {
+        return build(ResultCode.AUTH_FAIL, result);
     }
 
     public static <T> Result<T> permissionsNotEnough(T result) {
-        return build(ResultCode.PERMISSIONS_NOT_ENOUGH,result);
+        return build(ResultCode.PERMISSIONS_NOT_ENOUGH, result);
     }
 
     public static <T> Result<T> paramsError(T result) {
         return build(ResultCode.PARAMS_ERROR, result);
     }
-    public static <T> Result<T> unKnowError(T result){
-        return build(ResultCode.SYSTEM_UN_KNOW_ERROR,result);
+
+    public static <T> Result<T> unKnowError(T result) {
+        return build(ResultCode.SYSTEM_UN_KNOW_ERROR, result);
     }
+
     /**
      * 判断是否成功： 依据 Wrapper.SUCCESS_CODE == this.code
      *
