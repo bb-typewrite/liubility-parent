a1556b71e1fc7075bdec840a9f55fe5b6335c3bf
jdragon <1061917196@qq.com>
2021-12-27 23:32:24 +0800

    fix: MapStruct Integer to int NullPointerException

--- liubility-commons-dto/src/main/java/org/liubility/commons/dto/account/AccountDto.java
+++ liubility-commons-dto/src/main/java/org/liubility/commons/dto/account/AccountDto.java
@@ -23,11 +23,11 @@ public class AccountDto {
         this.password = password;
     }
 
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
